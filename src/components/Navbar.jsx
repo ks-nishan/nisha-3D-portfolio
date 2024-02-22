5b51db367b20be39893626950ba8b40682557b63
nishanthan <nishanthan@codevus.com>
2024-02-22 22:07:26 +0530

    impr: navigation icon added insted 2nd name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,8 +23,9 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white  text-[-18px] font-bold cursor-pointer">
-            Nishanthan <span className="sm:block hidden">| KAnagasunderam</span>
+          <p className="text-white  text-[18px] font-bold cursor-pointer flex">
+            Nishanthan&nbsp;
+            <span className="sm:block hidden">|&nbsp;💻</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">

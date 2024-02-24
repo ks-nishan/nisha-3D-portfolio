3c3ea88232a54af5ff65c4165eb67128bbe3c69a
nishanthan <nishanthan@codevus.com>
2024-02-24 22:22:25 +0530

    feature: long rectange navigator done

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
-// import React from "react";
-// import { moation } from "framer-motion";
+import React from "react";
+import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 
@@ -26,10 +26,28 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+      {/* long rectange navigator implementation */}
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.dev
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
+            />
+          </div>
+        </a>
+      </div>
     </section>
   );
 };
 
-{/* <br className="sm:block hidden" /> */}
+{
+  /* <br className="sm:block hidden" /> */
+}
 
 export default Hero;
